84b3745c2cc98a483ee6e18fdf0305943f5fb59b
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-28 10:21:34 +1030

    add  - - blank lines everytime play button is clicked

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,7 @@
 let image = document.getElementById("guess-image");
 let button = document.getElementById("play");
-let selectedWord = "";
+let word = document.getElementById("pokemon-name");
+
 let wordChoices = [
   "bulbasaur",
   "charizard",
@@ -9,16 +10,39 @@ let wordChoices = [
   "pikachu",
   "squirtle",
 ];
+// function main work staion //
 
-// function to select random word to be guessed and select the respected image
-
-function wordSelect() {
-  let index = Math.floor(Math.random() * wordChoices.length);
-  selectedWord = wordChoices[index];
+function playGame() {
+  // get a random word from the array
+  let selectedWord = getWord();
   console.log(selectedWord);
-  image.src = "./assets/images/" + selectedWord + ".jpeg";
+
+  let wordLength = selectedWord.length;
+
+  startGuess(selectedWord);
 }
 
+//fucntion to generate random word from the provided words
+function getWord() {
+  let index = Math.floor(Math.random() * wordChoices.length);
+  let takenWord = wordChoices[index];
+  image.src = "./assets/images/" + takenWord + ".jpeg";
+  return takenWord;
+}
+
+//function to guess the selected word
+
+function startGuess(guessWord) {
+  //split the word into an array of characters
+  let wordsArr = guessWord.split("");
+  //add - - for display
+  let displayArr = [];
+  for (i = 0; i < wordsArr.length; i++) {
+    displayArr.splice(i, 0, " _ ");
+  }
+  word.innerHTML = displayArr.join("");
+}
+// playgame button to start the game//
 button.addEventListener("click", function () {
-  wordSelect();
+  playGame();
 });
